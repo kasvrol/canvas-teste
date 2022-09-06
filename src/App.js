e3577be758707a48adf4719247cd53f384c1df3a
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-06 10:36:32 -0300

    feat: react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useEffect, useRef } from "react";
+
 function App() {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+
+    }, [])
 
     const startDrawing = () => { };
     const drawing = () => { };
