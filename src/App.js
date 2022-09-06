7460e9b18317c97dcbb1edc084cd2c653afa1e09
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-06 11:03:30 -0300

    feat: fuunção que capturará o click do mouse e mapeará o html iniciando a interação entre interface e usuario

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
     const canvasRef = useRef(null);
     const constextRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false)
 
     useEffect(() => {
         /*** CONFIGURAÇÃO DA TELA ***/
-        const canvas = canvasRef.current
-        canvas.width = window.innerWidth * 2
-        canvas.height = window.innerHeight * 2
-        canvas.style.height = `${window.innerHeight}px`
-        canvas.style.width = `${window.innerWidth}px`
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.height = `${window.innerHeight}px`;
+        canvas.style.width = `${window.innerWidth}px`;
 
-        const context = canvas.getContext("2d")
-        context.scale(2, 2)
-        context.lineCap = "round"
-        context.strokeStyle = "black"
-        context.lineWidth = "5px"
-        constextRef.current = context
-    }, [])
+        const context = canvas.getContext("2d");
+        context.scale(2, 2);
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = "5px";
+        constextRef.current = context;
+    }, []);
 
-    const startDrawing = () => { };
+    const reactCanvas = constextRef.current;
+
+    const startDrawing = (event) => {
+        const { offsetX, offsetY } = event;
+        reactCanvas.beginPath();
+        reactCanvas.moveTo(offsetX, offsetY);
+        setIsDrawing(true);
+    };
+
     const drawing = () => { };
     const finishDrawing = () => { };
 
     return (
-
         <canvas
             onMouseDown={startDrawing}
             onMouseMove={drawing}
@@ -33,7 +41,6 @@ function App() {
         >
             Canvas
         </canvas>
-
     );
 }
 
