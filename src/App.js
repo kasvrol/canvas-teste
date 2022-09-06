eb57c1f6284fc50c70f791110bc773691faff762
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-06 12:18:58 -0300

    feat: configurações para uso do canvas com a biblioteca react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 function App() {
-    return "canvas"
+    const canvasRef = useRef(null);
+
+    useLayoutEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.width = `${window.innerWidth}px`;
+        canvas.style.height = `${window.innerHeight}px`;
+
+        const context = canvas.getContext("2d");
+        context.scale(2, 2);
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = "5px";
+        constextRef.current = context;
+    }, [])
+
+    return <canvas style={{ background: 'red' }} ref={canvasRef}>
+
+    </canvas>
 }
 
 export default App;
