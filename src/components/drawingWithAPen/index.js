aeb4e7ef453db5c5addaf352d94c01399c1e488b
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-06 11:51:10 -0300

    refactor: mudança do arquivo que desenhará na tela para o diretorio components e restauração do app.js

---
+++ src/components/drawingWithAPen/index.js
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+
+function drawingWithAPen() {
+    const canvasRef = useRef(null);
+    const constextRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect(() => {
+        /*** CONFIGURAÇÃO DA TELA ***/
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.width = `${window.innerWidth}px`;
+        canvas.style.height = `${window.innerHeight}px`;
+
+        const context = canvas.getContext("2d");
+        context.scale(2, 2);
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = "5px";
+        constextRef.current = context;
+    }, []);
+
+    const startDrawing = (event) => {
+        const { clientX, clientY } = event;
+        constextRef.current.beginPath();
+        constextRef.current.moveTo(clientX, clientY);
+        setIsDrawing(true);
+    };
+
+    const drawing = (event) => {
+        if (!isDrawing) return;
+
+        const { clientX, clientY } = event;
+        constextRef.current.lineTo(clientX, clientY);
+        constextRef.current.stroke();
+    };
+
+    const finishDrawing = () => {
+        constextRef.current.closePath();
+        setIsDrawing(false);
+    };
+
+    return (
+        <canvas
+            ref={canvasRef}
+            onMouseDown={startDrawing}
+            onMouseMove={drawing}
+            onMouseUp={finishDrawing}
+        >
+            Canvas
+        </canvas>
+    );
+}
+
+export default drawingWithAPen;
