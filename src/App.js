96e4597cba567bb64b93ebd3d6871b6b70f8cd6b
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-13 17:24:16 -0300

    feat: id dos elementos em tela

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { FaRedo, FaUndo, FaSquare, FaCircle, FaPen } from "react-icons/fa";
 
 const generator = rough.generator();
 
-function createElement(x0, y0, x1, y1, elementType) {
+function createElement(id, x0, y0, x1, y1, elementType) {
     let roughElement;
     switch (elementType) {
         // case "pen":
@@ -16,10 +16,10 @@ function createElement(x0, y0, x1, y1, elementType) {
         //     return { x0, y0, x1, y1, roughElement };
         case "square":
             roughElement = generator.rectangle(x0, y0, x1 - x0, y1 - y0);
-            return { x0, y0, x1, y1, roughElement };
+            return { id, x0, y0, x1, y1, roughElement };
         default:
             roughElement = generator.line(x0, y0, x1, y1);
-            return { x0, y0, x1, y1, roughElement };
+            return { id, x0, y0, x1, y1, roughElement };
     }
 }
 
@@ -87,12 +87,12 @@ function App() {
             if (element) {
                 setSelectedElement(element)
                 setAction("moving");
-                console.log(element)
-                console.log(selectedElement)
             }
         } else {
             setAction("drawing");
+            const id = elements.length;
             const element = createElement(
+                id,
                 clientX,
                 clientY,
                 clientX,
@@ -109,6 +109,7 @@ function App() {
             const index = elements.length - 1;
             const { x0, y0 } = elements[index];
             const updadeElement = createElement(
+                index,
                 x0,
                 y0,
                 clientX,
@@ -119,7 +120,8 @@ function App() {
             elementsCopy[index] = updadeElement;
             setElements(elementsCopy);
         } else if (action === "moving") {
-            console.log("entrou na condicional")
+            const { id } = selectedElement
+            console.log("entrou na condicional", id)
         }
     };
 
