4a2f4520b042b07698ebf21e654c29ace4e7f856
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-15 11:05:07 -0300

    refactor: transferencia da função createElement para o arquivo createElement/index

---
+++ src/components/createElement/index.js
@@ -0,0 +1,21 @@
+import rough from "roughjs/bundled/rough.esm";
+
+const generator = rough.generator();
+
+export const createElement = (id, x0, y0, x1, y1, tool) => {
+    let roughElement;
+    switch (tool) {
+        case "line":
+            roughElement = generator.line(x0, y0, x1, y1);
+            return { id, x0, y0, x1, y1, roughElement };
+        case "ellipse":
+            roughElement = generator.ellipse(x0, y0, x1 - x0, y1 - y0);
+            return { id, x0, y0, x1, y1, roughElement };
+        case "rectangle":
+            roughElement = generator.rectangle(x0, y0, x1 - x0, y1 - y0);
+            return { id, x0, y0, x1, y1, roughElement };
+        default:
+            throw new Error(`Type not recognised: ${tool}`);
+    }
+}
+
