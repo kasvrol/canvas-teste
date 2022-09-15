45cb3eedea2c0bf63a93b17d62d59474e596386e
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-15 10:18:26 -0300

    refactor: transferencia da formula nearPoint para o arquivo forms/index

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -4,3 +4,21 @@ export const distance = (variableOne, variableTwo) => {
         Math.pow(variableOne.clientY - variableTwo.clientY, 2)
     );
 };
+
+export const nearPoint = (x0, y0, x1, y1, quadrant, shape) => {
+    let height
+    let width
+    if (shape === "rectangle") {
+        height = 11
+        width = 11
+    } else if (shape === "ellipse") {
+        height = 1
+        width = 1
+    }
+
+    else {
+        height = 7
+        width = 2
+    }
+    return Math.abs(x0 - x1) < width && Math.abs(y0 - y1) < height ? quadrant : null;
+};
