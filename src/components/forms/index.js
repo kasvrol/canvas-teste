3d4c8d9badf45bb97a80e27c117a4bff8e14759a
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-15 10:20:54 -0300

    refactor: transferencia da formula adjustElementCoordinates para o arquivo forms/index

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -22,3 +22,21 @@ export const nearPoint = (x0, y0, x1, y1, quadrant, shape) => {
     }
     return Math.abs(x0 - x1) < width && Math.abs(y0 - y1) < height ? quadrant : null;
 };
+
+export const adjustElementCoordinates = (element) => {
+    const { shape } = element.roughElement;
+    const { x0, x1, y0, y1 } = element;
+    if (shape === "rectangle" || shape === "ellipse") {
+        const minX = Math.min(x0, x1);
+        const maxX = Math.max(x0, x1);
+        const minY = Math.min(y0, y1);
+        const maxY = Math.max(y0, y1);
+        return { x0: minX, y0: minY, x1: maxX, y1: maxY };
+    } else {
+        if (x0 < x1 || (x0 === x1 && y0 < y1)) {
+            return { x0, y0, x1, y1 };
+        } else {
+            return { x0: x1, y0: y1, x1: x0, y1: y0 };
+        }
+    }
+};
