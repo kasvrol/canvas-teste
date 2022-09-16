d747e07294ed93426c182be5cedb7e0ae4c3f011
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-16 18:17:17 -0300

    feat: retorno do centro da elipse na função ellipseInsideAndBorder

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -38,24 +38,29 @@ function ellipseInsideAndBorder(clientX, clientY, x0, y0, x1, y1) {
     const clientRadius = { x: Math.abs(clientX - o.x), y: Math.abs(clientY - o.y) };
 
     const ellipseData = {
-        ellipseRadius, ellipseInside, border, clientRadius
+        ellipseRadius, ellipseInside, border, clientRadius, o
     }
 
     return ellipseData
 };
 
 export const ellipseInside = (clientX, clientY, x0, y0, x1, y1) => {
-    const a = Math.abs((x1 - x0) / 2); //distancia entre a origem e o vertice das abscissas 
-    const b = Math.abs((y1 - y0) / 2);//distancia entre a origem e o vertice das ordenadas
+    const ellipse = ellipseInsideAndBorder(clientX, clientY, x0, y0, x1, y1)
+    let insideOrBorder
 
-    const abs = centerX(x0, x1) + a; //ponto da origem na abscissa
-    const ord = centerY(y0, y1) + b;//ponto da origem na ordenada
-    const o = { abs: abs, ord: ord };
-    const x = Math.abs(clientX - o.abs);//distancia entre a origem e o clique do usuário
-    const y = Math.abs(clientY - o.ord);//distancia entre a origem e o clique do usuário
-    const xAndA = Math.pow(x, 2) / Math.pow(a, 2);
-    const yAndB = Math.pow(y, 2) / Math.pow(b, 2);
-    return Math.sqrt(xAndA + yAndB); //equação canônica ou reduzida da elipse
+    const inside = ellipse.clientRadius.x <= ellipse.ellipseRadius.x && ellipse.clientRadius.y <= ellipse.ellipseRadius.y ? true : false
+
+    if (ellipse.clientRadius.x <= ellipse.ellipseInside.x && ellipse.clientRadius.y <= ellipse.ellipseInside.y) {
+        insideOrBorder = 'inside'
+    } else if (ellipse.clientRadius.x <= ellipse.border.x && ellipse.clientRadius.y <= ellipse.border.y) {
+        insideOrBorder = 'border'
+    } else {
+        insideOrBorder = null
+    }
+
+    console.log(insideOrBorder)
+
+    return insideOrBorder
 };
 
 export const nearPoint = (x0, y0, x1, y1, quadrant, shape) => {
