9b5f12b67127709fd9bba0ffac89eae4efeb40f4
ana.karolina <ana.karolina@ablsystem.com.br>
2022-09-19 09:24:12 -0300

    fix: 'Uncaught Error' Type not recognised:

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
                     setAction("resizing");
                 }
             }
-        } else {
+        } else if (tool) {
             const id = elements.length;
             const element = createElement(
                 id,
